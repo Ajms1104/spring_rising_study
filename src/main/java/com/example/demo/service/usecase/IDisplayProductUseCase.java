3bf5bbd5f770f2a545ef81b5bbb440864363e209
Ajms1104 <164110764+Ajms1104@users.noreply.github.com>
2025-08-06 15:53:14 +0900

    0.7회차

---
+++ src/main/java/com/example/demo/service/usecase/IDisplayProductUseCase.java
@@ -0,0 +1,12 @@
+package com.example.demo.service.usecase;
+
+import com.example.demo.repository.entity.Product;
+import com.example.demo.service.IInputPort;
+
+import java.util.List;
+
+public interface IDisplayProductUseCase extends IInputPort {
+    List<Product> getProducts(String keyword);
+    Product getProductById(Long productId);
+
+}
